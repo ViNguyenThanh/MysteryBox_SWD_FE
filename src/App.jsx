d08441920b71384b26dd5ae8e1b2bd24691581fc
ViNguyenThanh <vint011103@gmail.com>
2024-05-28 07:08:25 +0700

    Navbar for admin page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Admin from './components/Admin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Admin/>
     </>
   )
 }
