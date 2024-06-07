73e340880d6e915226e608dccf7f3187d55f4946
LanNgoc2110 <ngocdl2110@gmail.com>
2024-06-07 11:56:41 +0700

    caption and step by step in home page

---
+++ src/components/Home/Caption/Caption.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Caption.css'
+
+const Caption = () => {
+    return (
+        <div className='caption-container'>
+            <p>Welcome to Mystery Box</p> <br/>
+            <p>
+                Explore a magical world with surprise gift boxes, specifically selected for your baby! 🎁✨ <br/>
+                Let your baby be surprised and happy every time they receive a gift from Mystery Box! 💝✨
+            </p>
+        </div>
+    )
+}
+
+export default Caption
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import Footer from '../../components/Footer/Footer'
 import Slide from '../../components/Home/Slide/Slide'
 import JoinUs from '../../components/Home/JoinUs/JoinUs'
 import Ranking from '../../components/Home/Ranking/Ranking'
+import Caption from '../../components/Home/Caption/Caption'
+import StepToBuyPackage from '../../components/Home/StepByStep/StepByStep'
 
 
 const Home = () => {
@@ -12,8 +14,10 @@ const Home = () => {
     <div className='container'>
       <Header />
       <div className="home-container">
+        <Caption/>
         <Slide />
         <JoinUs />
+        <StepToBuyPackage/>
         <Ranking />
         {/* <div className="content">
           <JoinUs />
